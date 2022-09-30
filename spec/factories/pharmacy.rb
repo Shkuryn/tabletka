165d4543b7ba64a861d8563d02f82bdf801766b6
igor shkuryn <shkuryn@gmail.com>
2022-09-30 23:05:01 +0400

    renamed_gps_to_lattitude

---
+++ spec/factories/pharmacy.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :pharmacy do
+    id { '1' }
+    title { 'pharma' }
+    city { 'Tbilisi' }
+    address { '9 march 2' }
+    phone { '11-22-33' }
+    gps { '41.705376, 44.789290' }
+    created_at  { 1.years.ago }
+    updated_at  { 1.years.ago }
+  end
+end
